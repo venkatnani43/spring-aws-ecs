d32986bf01b9b40a1bbe924ccc6daea0b44d4430
venkatnani43 <128156198+venkatnani43@users.noreply.github.com>
2025-05-30 22:30:13 +0530

    added code

--- src/main/java/com/java/CourseServiceApplication.java
+++ src/main/java/com/java/CourseServiceApplication.java
@@ -8,6 +8,7 @@ public class CourseServiceApplication {
 
 
     public static void main(String[] args) {
+    	System.out.println("app");
         SpringApplication.run(CourseServiceApplication.class, args);
     }
 
