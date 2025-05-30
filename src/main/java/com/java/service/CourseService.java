64fea6cddbfca615b2bb47327e25407bc7a9e667
venkatnani43 <128156198+venkatnani43@users.noreply.github.com>
2025-05-30 22:17:24 +0530

    added spring aws repo

---
+++ src/main/java/com/java/service/CourseService.java
@@ -0,0 +1,48 @@
+package com.java.service;
+
+import org.springframework.stereotype.Service;
+
+import com.java.dto.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+
+    //RDS DB
+    private final List<Course> courses = new ArrayList<>();
+
+    // Create a new course
+    public void addCourse(Course course) {
+        courses.add(course);
+    }
+
+    // Retrieve all courses
+    public List<Course> getAllCourses() {
+        return courses;
+    }
+
+    // Retrieve a course by id
+    public Optional<Course> getCourseById(int id) {
+        return courses.stream()
+                .filter(course -> course.getId() == id)
+                .findFirst();
+    }
+
+    // Update a course
+    public boolean updateCourse(int id, Course newCourse) {
+        return getCourseById(id).map(existingCourse -> {
+            courses.remove(existingCourse);
+            courses.add(newCourse);
+            return true;
+        }).orElse(false);
+    }
+
+    // Delete a course by id
+    public boolean deleteCourse(int id) {
+        return courses
+                .removeIf(course -> course.getId() == id);
+    }
+}
